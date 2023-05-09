4180461b746715da0814deaded5abda5cee0f83a
john ontita <johnontita2000@gmail.com>
2023-05-09 06:43:40 +0300

    dynamic typing together with some assignment operators

---
+++ app.js
@@ -0,0 +1,22 @@
+//dynamic typing is achieved by using the declared vaiable without beginning with 'let' 
+let myDadIsLoving=true;
+console.log(typeof myDadIsLoving);
+myDadIsLoving='yes!'
+console.log(typeof myDadIsLoving);
+
+//basic operators
+const businessCapital = 200000;
+const fixedAssets=400000;
+console.log(businessCapital*2,fixedAssets+10000);
+const startingYear= 'i started my business in the year 2013' 
+//exponential operations we use double*
+console.log(fixedAssets**2)
+console.log(startingYear + ' ' + 'later my brother joined also ')
+
+//assignment operators
+let x=20-5;
+x+=10;
+x*=4;
+console.log(x)
+//comparison operators
+console.log(businessCapital>fixedAssets);
